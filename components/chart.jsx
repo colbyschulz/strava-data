7ec49be19d0ca91382e5877e5ddae463344243bb
Colby Schulz <colbyschulz@Colbys-MacBook-Pro.local>
2021-01-02 22:07:45 -0500

    initial repo push

---
+++ components/chart.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 400px;
+  background-color: yellow;
+  align-items: center;
+  justify-content: center;
+`
+const Chart = () => {
+  return (
+    <Wrapper>
+      CHART GOES HERE
+    </Wrapper>
+  )
+}
+
+export default Chart;
